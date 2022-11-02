a23093fa873e8139925ee8bd70d8a6a1bd8372ce
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-02 21:53:09 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -2,19 +2,19 @@ const refs = {
     valueEl: document.querySelector('#value'),
     incrementBtn: document.querySelector('button[data-action="increment"]'),
     decrementBtn: document.querySelector('button[data-action="decrement"]'),
-}
+};
 
 const clickFunction = {
     value: 0,
     incMetod() {
-        this.value += 1; 
+        this.value += 1;
         return refs.valueEl.textContent = this.value;
     },
     decMetod() {
-        this.value -= 1; 
+        this.value -= 1;
         return refs.valueEl.textContent = this.value;
     },
-}
+};
 
-refs.incrementBtn.addEventListener('click', clickFunction.incMetod);
-refs.decrementBtn.addEventListener('click', clickFunction.decMetod);
+refs.incrementBtn.addEventListener('click', clickFunction.incMetod.bind(clickFunction));
+refs.decrementBtn.addEventListener('click', clickFunction.decMetod.bind(clickFunction));
