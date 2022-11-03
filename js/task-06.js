c9d39cc84ca458e2307b23e9ad17024dea23b1b8
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-03 16:19:28 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,28 @@
 const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener('blur', event =>
-    inputEl.value.length === 6
-        ? inputEl.classList.add('valid')
-        : inputEl.classList.add('invalid')
-);
-    console.dir(inputEl);
+inputEl.addEventListener('focus', validatorFocus);
+inputEl.addEventListener('blur', validatorBlur);
+
+function validatorFocus(event) {
+    console.log('focus');
+
+    // event.preventDefault();
+        
+}
+function validatorBlur(event) {
+    console.log('blur');
+    console.dir(inputEl.dataset.length);
+    console.dir(inputEl.value.length);
+
+    if (Number(inputEl.value.length) !== Number(inputEl.dataset.length)) {
+        inputEl.classList.remove('valid')
+        inputEl.classList.add('invalid')
+    } else {
+        inputEl.classList.remove('invalid')
+        inputEl.classList.add('valid')
+    }
+
+    // Number(inputEl.value.length) !== Number(inputEl.dataset.length)
+    //     ? inputEl.classList.toggle('invalid')
+    //     : inputEl.classList.toggle('valid') 
+}
