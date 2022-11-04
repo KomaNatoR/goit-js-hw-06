ab52ce26328e9c9d896638c4fba814698809697e
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-04 07:59:05 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+const refs = {
+    inputEl: document.querySelector('#font-size-control'),
+    textEl: document.querySelector('#text'),
+}
+
+refs.inputEl.addEventListener('input', changeFontSize);
+
+function changeFontSize(event) {
+    // const px = `${refs.inputEl.value}px`;
+    // console.dir(px);
+    refs.textEl.style.fontSize = `${refs.inputEl.value}px`;
+    // console.dir(`${refs.inputEl.value}px`);
+}
