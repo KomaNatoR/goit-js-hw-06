368fa24980f64f2303dbad9f75feff78f86cb2ee
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-04 14:47:58 +0200

    done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  spanEl: document.querySelector('.color'),
+  spanBtn: document.querySelector('.change-color'),
+}; 
+
+refs.spanBtn.addEventListener('click', onBntChangeColor);
+// refs.bodyEl.addEventListener('click', onBodyChangeColor);
+
+function onBntChangeColor(e) {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanBtn.textContent = getRandomHexColor();
+  // console.log(getRandomHexColor());
+}
+// function onBodyChangeColor(e) {
+//   e.currentTarget.style.backgroundColor = getRandomHexColor();
+// }
+
+// console.log(refs.bodyEl);
